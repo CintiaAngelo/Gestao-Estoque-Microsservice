17e7650107a969e9315695c9b13658b19107f378
labsfiap <labsfiap@L1710MICRO28>
2025-10-29 20:29:50 -0300

    Add project files.

---
+++ Domain/MovimentacaoEstoque.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum TipoMovimentacao
+    {
+        ENTRADA,
+        SAIDA
+    }
+    public class MovimentacaoEstoque
+    {
+        public int Id { get; set; }
+        public TipoMovimentacao Tipo { get; set; }
+        public int Quantidade { get; set; }
+        public string DataMovimentacao { get; set; }
+        public string? Lote { get; set; }
+        public string? DataValidade { get; set; }
+        public int ProdutoId { get; set; }
+    }
+}
---
+++ Domain/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+        public enum Categoria
+        {
+            PERECIVEL,
+            NAO_PERECIVEL
+        }
+        public class Produto
+        {
+            public string CodigoSKU { get; set; }
+            public string Nome { get; set; }
+            public Categoria Categoria { get; set; }
+            public decimal PrecoUnitario { get; set; }
+            public int QuantidadeMinima { get; set; }
+            public string DataCriacao { get; set; }
+        }
+
+}
+
